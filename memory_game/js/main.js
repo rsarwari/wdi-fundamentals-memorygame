7bccaa288e83680b1191a7afd925d688c0bbc262
Ridwan sarwari <r.sarwari@vigilvideo.com>
2019-03-04 23:42:58 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("up and running");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+//console.log("Use Flipped" + " " + cardOne + " and " + cardThree);
+console.log("Use Flipped" + " " + cardOne);
+console.log("Use Flipped" + " " + cardThree);
