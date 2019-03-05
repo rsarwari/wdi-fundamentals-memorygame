1bacfba47116a46781d90de2c50da2a53db6a789
Ridwan sarwari <r.sarwari@vigilvideo.com>
2019-03-05 13:52:59 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,8 +10,8 @@ console.log("Use Flipped" + " " + cardOne);
 console.log("Use Flipped" + " " + cardThree);
 */
 
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
+//var cards = ["queen", "queen", "king", "king"];
+//var cardsInPlay = [];
 
 /*
 	var cardOne = cards[0];
@@ -34,6 +34,29 @@ var cardsInPlay = [];
 		console.log('0');
 	}
 */
+
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}];
+
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	console.log("You found a match!");
@@ -42,9 +65,11 @@ var checkForMatch = function(){
 	}
 }
 var flipCard = function(cardId){
-	console.log("user flipped " + cards[cardId]);
-	cardsInPlay.push(cardId);
-	checkForMatch();
+	console.log("user flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	//cardsInPlay.push(cards[cardId]);
+	//checkForMatch();
 };
 flipCard(0);
 flipCard(2);
